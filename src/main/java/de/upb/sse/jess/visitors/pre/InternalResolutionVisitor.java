73ff05416d4d82fb0312c936a22a6b22d9090edb
Stefan Schott <stefan.schott@gmx.net>
2024-11-27 15:41:44 +0100

    Revert "investigation of issue where compiling methods and fields at the same time leads to only the fields being compiled"
    This reverts commit ac6ad885cd0e61dee0b78431b06a26890a33bbe1.

--- src/main/java/de/upb/sse/jess/visitors/pre/InternalResolutionVisitor.java
+++ src/main/java/de/upb/sse/jess/visitors/pre/InternalResolutionVisitor.java
@@ -247,12 +247,9 @@ public class InternalResolutionVisitor extends VoidVisitorAdapter<Void> {
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     private boolean isWithinKeptMethod(Node node) {
+        if (isKeptClinit(node)) return isWithinClinit(node);
+        if (isKeptInit(node)) return isWithinInit(node);
         if (isKeptMethod(node)) return true;
-
-
-//        if (isKeptClinit(node)) return isWithinClinit(node);
-//        if (isKeptInit(node)) return isWithinInit(node);
-
         if (!looseMatching) return isWithinKeptMethodBasedOnPartialSignature(node);
 
         Optional<CallableDeclaration> declaredMethodOpt = node.findAncestor(CallableDeclaration.class);
